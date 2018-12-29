606caef28b659d3c518b2aec80a2d42cc0a91eda
SangWoo <sangw0804@likelion.org>
2018-12-30 01:32:19 +0900

    insert 메소드 작성 & 테스트

--- test/heap.test.js
+++ test/heap.test.js
@@ -1,12 +1,29 @@
 import expect from 'expect';
 import { Heap } from '../lib';
+import { INVALID_VALUE } from '../lib/error';
 
 let emptyMaxHeap;
 let emptyMinHeap;
+let multiMaxHeap;
+let multiMinHeap;
 
 beforeEach(() => {
   emptyMaxHeap = new Heap();
   emptyMinHeap = new Heap(false);
+  multiMaxHeap = new Heap();
+  multiMaxHeap
+    .insert(10)
+    .insert(20)
+    .insert(5)
+    .insert(15)
+    .insert(1);
+  multiMinHeap = new Heap(false);
+  multiMinHeap
+    .insert(10)
+    .insert(20)
+    .insert(5)
+    .insert(15)
+    .insert(1);
 });
 
 describe('MaxHeap Test', () => {
@@ -17,12 +34,55 @@ describe('MaxHeap Test', () => {
       expect(emptyMaxHeap.max).toBe(true);
     });
   });
+
+  // insert 메소드 테스트
+  describe('MaxHeap - insert', () => {
+    // 숫자가 value로 주어지면 맞는 위치에 insert 한다.
+    it('should insert value in correct position', () => {
+      emptyMaxHeap.insert(5);
+      expect(emptyMaxHeap.values[0]).toBe(5);
+      emptyMaxHeap.insert(3);
+      expect(emptyMaxHeap.values[1]).toBe(3);
+      emptyMaxHeap.insert(10);
+      expect(emptyMaxHeap.values[0]).toBe(10);
+      expect(emptyMaxHeap.size()).toBe(3);
+    });
+
+    // 숫자가 이닌 값이 value 로 주어지면 에러를 발생시킨다.
+    it('should throw error if value is not a number', () => {
+      expect(() => emptyMaxHeap.insert('1')).toThrow(INVALID_VALUE);
+      expect(emptyMaxHeap.size()).toBe(0);
+      expect(emptyMaxHeap.max).toBe(true);
+    });
+  });
 });
 
 describe('MinHeap Test', () => {
   // MinHeap 초기화
   describe('MinHeap - initialize', () => {
     it('should make empty minHeap', () => {
+      expect(emptyMinHeap.size()).toBe(0);
+      expect(emptyMinHeap.max).toBe(false);
+    });
+  });
+
+  // insert 메소드 테스트
+  describe('MinHeap - insert', () => {
+    // 숫자가 value로 주어지면 맞는 위치에 insert 한다.
+    it('should insert value in correct position', () => {
+      emptyMinHeap.insert(5);
+      expect(emptyMinHeap.values[0]).toBe(5);
+      emptyMinHeap.insert(3);
+      expect(emptyMinHeap.values[1]).toBe(5);
+      emptyMinHeap.insert(10);
+      expect(emptyMinHeap.values[0]).toBe(3);
+      expect(emptyMinHeap.values[2]).toBe(10);
+      expect(emptyMinHeap.size()).toBe(3);
+    });
+
+    // 숫자가 이닌 값이 value 로 주어지면 에러를 발생시킨다.
+    it('should throw error if value is not a number', () => {
+      expect(() => emptyMinHeap.insert('1')).toThrow(INVALID_VALUE);
       expect(emptyMinHeap.size()).toBe(0);
       expect(emptyMinHeap.max).toBe(false);
     });
