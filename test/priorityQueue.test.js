85adcf97984d6971735fc78d1cb21d11f657c364
SangWoo <sangw0804@likelion.org>
2018-12-30 17:09:35 +0900

    dequeue 메소드 작성 & 테스트

--- test/priorityQueue.test.js
+++ test/priorityQueue.test.js
@@ -3,9 +3,16 @@ import { PriorityQueue } from '../lib';
 import { INVALID_VALUE } from '../lib/error';
 
 let emptyPriorityQueue;
+let multiPriorityQueue;
 
 beforeEach(() => {
   emptyPriorityQueue = new PriorityQueue();
+  multiPriorityQueue = new PriorityQueue();
+  multiPriorityQueue.enqueue('D', 4);
+  multiPriorityQueue.enqueue('E', 5);
+  multiPriorityQueue.enqueue('B', 2);
+  multiPriorityQueue.enqueue('C', 3);
+  multiPriorityQueue.enqueue('A', 1);
 });
 
 describe('Priority Queue Test', () => {
@@ -36,6 +43,24 @@ describe('Priority Queue Test', () => {
     // 우선순위가 number 가 아닐 경우 에러를 발생시킨다.
     it('should throw error if priority is not a number', () => {
       expect(() => emptyPriorityQueue.enqueue('A', true)).toThrow(INVALID_VALUE);
+      expect(emptyPriorityQueue.size()).toBe(0);
+    });
+  });
+
+  // dequeue 테스트
+  describe('Priority Queue - dequeue', () => {
+    // 가장 우선순위가 낮은 노드를 리턴한다.
+    it('should return node with lowest priority', () => {
+      expect(multiPriorityQueue.dequeue().value).toBe('A');
+      expect(multiPriorityQueue.dequeue().value).toBe('B');
+      expect(multiPriorityQueue.dequeue().value).toBe('C');
+      expect(multiPriorityQueue.dequeue().value).toBe('D');
+      expect(multiPriorityQueue.dequeue().value).toBe('E');
+    });
+
+    // 빈 priority queue 의 경우 undefined를 리턴한다.
+    it('should return undefined if empty', () => {
+      expect(emptyPriorityQueue.dequeue()).toBeUndefined();
       expect(emptyPriorityQueue.size()).toBe(0);
     });
   });
