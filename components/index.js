b5cf3d3ec53a93d00e16363c2c32c7bd45a6022b
SangWoo <sangw0804@likelion.org>
2018-12-30 18:33:19 +0900

    components 디렉토리 생성

---
+++ components/index.js
@@ -0,0 +1,1 @@
+export { SllComponent } from './singlyLinkedList/singlyLinkedList';
---
+++ components/singlyLinkedList/singlyLinkedList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SinglyLinkedList } from '../../lib';
+
+const sllComponent = props => {
+  return <div>sllComponent</div>;
+};
+
+export { sllComponent };
