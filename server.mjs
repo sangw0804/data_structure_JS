63e4b907645bdfa552d704244ec9deda0931fd2c
SangWoo <sangw0804@likelion.org>
2019-01-01 21:21:33 +0900

    public 디렉토리 생성

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS/public'));
 app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS'));
 
 app.listen(3000, () => {
