66786f1b7b1757065093d6833d255b9419ec5828
SangWoo <sangw0804@likelion.org>
2019-01-01 22:00:41 +0900

    server.mjs 리팩토링

--- server.mjs
+++ server.mjs
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS/public'));
 app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS'));
 
-app.listen(3000, () => {
-  console.log('server is listening');
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is listening at ${port}`);
 });
