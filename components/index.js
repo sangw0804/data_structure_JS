ef7256e326b38de01e6398953315ae4dc89a4dda
SangWoo <sangw0804@likelion.org>
2019-01-01 17:00:44 +0900

    visualize priority Queue

--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { createStackElement } from './stack/stack.js';
 export { createQueueElement } from './queue/queue.js';
 export { createDequeueElement } from './dequeue/dequeue.js'
 export { createHeapElement } from './heap/heap.js';
+export { createPriorityQueueElement } from './priorityQueue/priorityQueue.js';
--- components/node.js
+++ components/node.js
@@ -10,4 +10,25 @@ const createNodeElement = value => {
   return nodeElement;
 };
 
-export { createNodeElement };
+const createPriorityNodeElement = (value, priority) => {
+  const priorityNodeElement = document.createElement('div');
+
+  priorityNodeElement.setAttribute(
+    'style',
+    'border-radius:10px; background-color: tomato; height: 100px; width:100px; display: inline-flex; flex-direction:column; justify-content: around; align-items:center; margin: 5px; text-align: center'
+  );
+
+  const priorityNode = document.createElement('div');
+  priorityNode.setAttribute('style', 'font-size: 1.3rem; margin: 10px');
+  priorityNode.innerText = priority;
+
+  const valueNode = document.createElement('div');
+  valueNode.setAttribute('style', 'font-size: 1.7rem; margin: 10px');
+  valueNode.innerText = value;
+
+  priorityNodeElement.appendChild(priorityNode);
+  priorityNodeElement.appendChild(valueNode);
+  return priorityNodeElement;
+};
+
+export { createNodeElement, createPriorityNodeElement };
