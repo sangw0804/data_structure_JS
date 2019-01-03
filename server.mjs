3171c4761ab26da441f99eb802ddcde913590dab
SangWoo <sangw0804@likelion.org>
2019-01-03 13:02:41 +0900

    ejs

--- server.mjs
+++ server.mjs
@@ -1,9 +1,26 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS/public'));
 app.use(express.static('/Users/sangwoo/Desktop/programming/dataStructureJS'));
+
+app.get('/:data_structure', (req, res) => {
+  try {
+    const dataStructure = req.params.data_structure;
+
+    res.render(`${dataStructure}`);
+  } catch (err) {
+    res.render('bad', { err });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 const port = 3000;
 app.listen(port, () => {
