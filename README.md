c151965f583472c470d3edc0f0114e40205d805f
SangWoo <sangw0804@likelion.org>
2019-01-03 23:57:57 +0900

    최종

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 - jest
 - expect
 - babel-preset-env
+
+# TEST
+
+루트 디렉토리에서 npm run test 로 테스트 할 수 있습니다.
+
+# SERVER LISTENING
+
+루트 디렉토리에서 npm run start 로 visualization 웹 서버를 3000번 포트에 실행시킬 수 있습니다.
--- components/heap/heap.js
+++ components/heap/heap.js
@@ -3,20 +3,23 @@ import { createNodeElement, createEmptyNodeElement } from '../node.js';
 const createHeapElement = heap => {
   const result = document.createElement('div');
   result.setAttribute('id', 'heap');
-  result.setAttribute('style', 'display:flex; flex-direction: column; align-items: center');
+  result.setAttribute('style', 'display:flex; flex-direction: column; align-items: center; justify-content: around');
 
   let end = 0;
   let power = 1;
   let row = document.createElement('div');
+
   heap.values.forEach((value, index) => {
     if (index === end) {
-      row.appendChild(createNodeElement(value));
+      const node = createNodeElement(value);
+      row.appendChild(node);
       result.appendChild(row);
       row = document.createElement('div');
       power *= 2;
       end += power;
     } else {
-      row.appendChild(createNodeElement(value));
+      const node = createNodeElement(value);
+      row.appendChild(node);
     }
   });
 
