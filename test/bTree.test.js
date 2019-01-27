e406ba8f21a5374b4bcb35da2af09268af64c96e
SangWoo <sangw0804@likelion.org>
2019-01-27 17:44:25 +0900

    find 메소드 작성 & 테스트

--- test/bTree.test.js
+++ test/bTree.test.js
@@ -11,59 +11,29 @@ beforeEach(() => {
 
   oneFloorBTree = new BTree(5);
   oneFloorBTree.root = new BTreeNode(oneFloorBTree.limit);
-  oneFloorBTree.root.values = ['a', 'b', 'c', 'e', 'f'];
-  oneFloorBTree.root.children = ['1', '2', '3', '4', '5', '6'];
-  oneFloorBTree.root.valueLength = 5;
+  oneFloorBTree
+    .insert('a')
+    .insert('b')
+    .insert('c')
+    .insert('d');
 
   twoFloorBTree = new BTree(5);
-  twoFloorBTree.root = new BTreeNode(twoFloorBTree.limit);
-  twoFloorBTree.root.values = ['c', 'j'];
-  twoFloorBTree.root.valueLength = 2;
-  const firstChild = new BTreeNode(twoFloorBTree.limit);
-  firstChild.values = ['a', 'b'];
-  firstChild.children = ['1', '2', '3'];
-  firstChild.valueLength = 2;
-  const secondChild = new BTreeNode(twoFloorBTree.limit);
-  secondChild.values = ['d', 'e', 'f', 'g', 'h'];
-  secondChild.children = ['1', '2', '3', '4', '5', '6'];
-  secondChild.valueLength = 5;
-  const thirdChild = new BTreeNode(twoFloorBTree.limit);
-  thirdChild.values = ['k', 'l'];
-  thirdChild.children = ['1', '2', '3'];
-  thirdChild.valueLength = 2;
-  twoFloorBTree.root.children = [firstChild, secondChild, thirdChild];
+  twoFloorBTree
+    .insert('a')
+    .insert('b')
+    .insert('c')
+    .insert('d')
+    .insert('e')
+    .insert('f')
+    .insert('g')
+    .insert('h')
+    .insert('i');
 });
 
 describe('B-Tree Test', () => {
   describe('Initialize B-Tree', () => {
     it('should create empty B-Tree', () => {
       expect(emptyBTree.root).toBeNull();
-    });
-  });
-
-  describe('_split Test', () => {
-    it('should split root node and return new node reference', () => {
-      oneFloorBTree.root = oneFloorBTree._split(null, 'd');
-      expect(oneFloorBTree.root.values).toEqual(['c']);
-      expect(oneFloorBTree.root.size()).toBe(1);
-      expect(oneFloorBTree.root.children[0].values).toEqual(['a', 'b']);
-      expect(oneFloorBTree.root.children[0].children).toEqual(['1', '2', '3']);
-      expect(oneFloorBTree.root.children[1].values).toEqual(['e', 'f']);
-      expect(oneFloorBTree.root.children[1].children).toEqual(['4', '5', '6']);
-    });
-
-    it('should split normal node and return reference of parent node', () => {
-      twoFloorBTree.root = twoFloorBTree._split(twoFloorBTree.root, 'i');
-      expect(twoFloorBTree.root.values).toEqual(['c', 'f', 'j']);
-      expect(twoFloorBTree.root.size()).toBe(3);
-      expect(twoFloorBTree.root.children[0].values).toEqual(['a', 'b']);
-      expect(twoFloorBTree.root.children[0].children).toEqual(['1', '2', '3']);
-      expect(twoFloorBTree.root.children[1].values).toEqual(['d', 'e']);
-      expect(twoFloorBTree.root.children[1].children).toEqual(['1', '2', '3']);
-      expect(twoFloorBTree.root.children[2].values).toEqual(['g', 'h']);
-      expect(twoFloorBTree.root.children[2].children).toEqual(['4', '5', '6']);
-      expect(twoFloorBTree.root.children[3].values).toEqual(['k', 'l']);
-      expect(twoFloorBTree.root.children[3].children).toEqual(['1', '2', '3']);
     });
   });
 
@@ -163,13 +133,17 @@ describe('B-Tree Test', () => {
     });
   });
 
-  // describe('Find Test', () => {
-  //   it('should return false if given value not in Tree', () => {
-  //     expect(fulledBTree.find(100)).toBe(false);
-  //   });
+  describe('Find Test', () => {
+    it('should return false if given value not in Tree', () => {
+      expect(oneFloorBTree.find('z')).toBe(false);
+      expect(twoFloorBTree.find('z')).toBe(false);
+    });
 
-  //   it('should return containing node if given value is found in Tree', () => {
-  //     expect(fulledBTree.find(5).findValue(5)).toBeGreaterThan(-1);
-  //   });
-  // });
+    it('should return containing node if given value is found in Tree', () => {
+      expect(oneFloorBTree.find('c').findValue('c')).toBeGreaterThan(-1);
+      expect(twoFloorBTree.find('h').findValue('h')).toBeGreaterThan(-1);
+    });
+  });
+
+  describe();
 });
