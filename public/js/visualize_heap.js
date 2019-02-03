1b0ed9af615240df39ae0b705f3b03131d5c6408
SangWoo <sangw0804@likelion.org>
2019-02-03 20:38:42 +0900

    heap animation 추가

--- public/js/visualize_heap.js
+++ public/js/visualize_heap.js
@@ -1,5 +1,6 @@
 import { Heap } from '../index.js';
 import { createHeapElement } from '../components/index.js';
+import { delayAndApply, buttonDisableHOC } from './helpers/index.js';
 
 const heapName = { true: 'Max Heap', false: 'Min Heap' };
 let isMaxHeap = true;
@@ -10,24 +11,36 @@ const name = document.getElementById('name');
 // insert
 const insertButton = document.getElementById('insert_button');
 const insertInput = document.getElementById('insert_value');
-insertButton.onclick = () => {
+insertButton.onclick = buttonDisableHOC(async () => {
   try {
     if (!insertInput.value.length) throw new Error('채워지지 않은 필드가 있습니다.');
-    heap.insert(+insertInput.value);
-    main.innerHTML = null;
-    main.appendChild(createHeapElement(heap));
+    heap.insert(+insertInput.value, true);
+
+    const snapshots = heap.returnSnapshots();
+    for (let i = 0; i < snapshots.length; i += 1) {
+      await delayAndApply(main, createHeapElement(snapshots[i]), 1000);
+    }
+    await delayAndApply(main, createHeapElement(heap), 1000);
   } catch (e) {
     alert(e);
   }
-};
+}, 'INSERT');
 
 // pop
 const popButton = document.getElementById('pop_button');
-popButton.onclick = () => {
-  heap.pop();
-  main.innerHTML = null;
-  main.appendChild(createHeapElement(heap));
-};
+popButton.onclick = buttonDisableHOC(async () => {
+  try {
+    heap.pop(true);
+
+    const snapshots = heap.returnSnapshots();
+    for (let i = 0; i < snapshots.length; i += 1) {
+      await delayAndApply(main, createHeapElement(snapshots[i]), 1000);
+    }
+    await delayAndApply(main, createHeapElement(heap), 1000);
+  } catch (e) {
+    alert(e);
+  }
+}, 'POP');
 
 // minHeap / maxHeap change
 const changeButton = document.getElementById('change_button');
